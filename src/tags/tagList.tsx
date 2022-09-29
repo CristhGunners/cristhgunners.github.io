3cfeaa3864beec16394fd8628c050f46e2e89685
CristhGunners <gunners_one@hotmail.com>
2022-09-28 22:10:43 -0300

    feat: Update tag list style

--- src/tags/tagList.tsx
+++ src/tags/tagList.tsx
@@ -9,10 +9,10 @@ export type ITagListProps = {
 };
 
 const TagList = (props: ITagListProps) => (
-  <div className="w-full sm:columns-2 lg:columns-3 gap-6">
+  <div className="w-full sm:grid sm:overflow-hidden sm:grid-cols-2 sm:grid-rows-2 sm:gap-4">
     {props.tags.map((tag) => (
       <Link href={'/tags/[slug]'} as={`/tags/${tag.tag}`} key={tag.tag}>
-        <a className="bg-gray-200 p-2 rounded inline-block w-full mb-6 hover:bg-gray-300">
+        <a className="bg-gray-200 p-2 rounded inline-block w-full mb-4 hover:bg-gray-300 sm:mb-0">
           <span className="flex items-center space-x-2">
             <span className="bg-pink-600 flex items-center justify-center text-white font-extrabold rounded h-8 w-8 text-sm">
               {tag.count}
