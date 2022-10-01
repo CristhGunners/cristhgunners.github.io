f46371e4fb9fee2c5320212fb7e6825130fa47f6
CristhGunners <gunners_one@hotmail.com>
2022-10-01 12:56:13 -0300

    fix: Fix list style in about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -7,7 +7,7 @@ import { Main } from '../templates/Main';
 const About = () => (
   <Main meta={<Meta title="Acerca de Mi" description="Acerca de Mi" />}>
     <Content>
-      <ul className="mx-auto my-6 max-w-sm text-left list-disc marker:text-pink-600">
+      <ul className="mx-auto my-6 max-w-sm text-left sm:list-disc sm:marker:text-pink-600">
         <li>🧑‍💻 Cristhofer Andana Alcaino</li>
         <li>🚀 Desarrollador de Software</li>
         <li>🇨🇱 Chile</li>
