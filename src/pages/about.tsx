8c8465e32544faa0b9a1dca339d9fad7c6c4cc8b
CristhGunners <gunners_one@hotmail.com>
2023-07-30 20:08:24 -0400

    feat: Use icons svg in about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -7,12 +7,47 @@ import { Main } from '../templates/Main';
 const About = () => (
   <Main meta={<Meta title="Acerca de Mi" description="Acerca de Mi" />}>
     <Content>
-      <ul className="mx-auto my-6 max-w-sm text-left text-gray-700 sm:list-disc sm:marker:text-pink-600 dark:text-white">
-        <li>🧑‍💻 Cristhofer Andana Alcaino</li>
-        <li>🚀 Desarrollador de Software</li>
-        <li>🇨🇱 Chile</li>
-        <li>🏀 76ers</li>
-        <li>⚽ Arsenal F.C.</li>
+      <ul className="mx-auto my-6 max-w-sm text-left text-gray-700 space-y-2 list-none dark:text-white">
+        <li className="flex items-center justify-start space-x-2">
+          <img
+            src={`/assets/images/about/me.svg`}
+            alt="Cristhofer Andana Alcaino"
+            className="w-6 h-6"
+          />{' '}
+          <span>Cristhofer Andana Alcaino</span>
+        </li>
+        <li className="flex items-center justify-start space-x-2">
+          <img
+            src={`/assets/images/about/rocket.svg`}
+            alt="Desarrollador de Software"
+            className="w-6 h-6"
+          />{' '}
+          <span>Desarrollador de Software</span>
+        </li>
+        <li className="flex items-center justify-start space-x-2">
+          <img
+            src={`/assets/images/about/chile.svg`}
+            alt="Chile"
+            className="w-6 h-6"
+          />{' '}
+          <span>Chile</span>
+        </li>
+        <li className="flex items-center justify-start space-x-2">
+          <img
+            src={`/assets/images/about/basketball.svg`}
+            alt="76ers"
+            className="w-6 h-6"
+          />{' '}
+          <span>76ers</span>
+        </li>
+        <li className="flex items-center justify-start space-x-2">
+          <img
+            src={`/assets/images/about/soccer.svg`}
+            alt="Arsenal F.C."
+            className="w-6 h-6"
+          />{' '}
+          <span>Arsenal F.C.</span>
+        </li>
       </ul>
     </Content>
   </Main>
