bce88b13ffd0fefa027fd6200e0026ea3bef980d
CristhGunners <gunners_one@hotmail.com>
2024-01-04 23:29:25 -0300

    fix: Fix menu mobile spacing

--- src/navigation/Navbar.tsx
+++ src/navigation/Navbar.tsx
@@ -5,7 +5,7 @@ type INavbarProps = {
 };
 
 const Navbar = (props: INavbarProps) => (
-  <ul className="navbar flex flex-col items-center justify-center text-xl sm:flex-row sm:flex-wrap sm:justify-start sm:space-x-6">
+  <ul className="navbar flex flex-col items-center justify-center space-y-4 text-xl sm:flex-row sm:flex-wrap sm:justify-start sm:space-x-6 sm:space-y-0">
     {props.children}
   </ul>
 );
